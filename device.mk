692a10ff72488645982b79910275f4993a11bfb9
Atman <masteratman@gmail.com>
2019-04-25 10:12:13 +0000

    violet: Build Camera HIDL
    Change-Id: I7a8d27ac4b9d3341c2097456609e713a21e7a488

--- device.mk
+++ device.mk
@@ -32,6 +32,17 @@ PRODUCT_PACKAGES += \
 
 
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl:64 \
+    android.hardware.camera.provider@2.4-service_64 \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libxml2 \
+    Snap \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
+
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
