96b624f25d5d248a0d8ec36ffa7056f20b5faec5
PIPIPIG233666 <2212848813@qq.com>
2019-09-14 23:00:29 -0400

    sm6150-common: Build audio stack from source [1/2]
    Change-Id: If1f0f7f79d99e8dca4061146cf5e589fcdfd8769

--- device.mk
+++ device.mk
@@ -31,6 +31,30 @@ TARGET_SCREEN_WIDTH := 1080
 
 
 
+
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
+    audio.a2dp.default \
+    audio.primary.sm6150:32 \
+    audio.r_submix.default \
+    audio.usb.default \
+    liba2dpoffload \
+    libaudio-resampler \
+    libhdmiedid \
+    libhfp \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libsndmonitor \
+    libspkrprot \
+    libtinycompress \
+    libtinycompress.vendor \
+    libvolumelistener
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
