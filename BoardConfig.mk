ee0baebf337f95e0de5111155a19eb5ae7d4df8b
Haky86 <hakamelassouad@gmail.com>
2021-09-03 21:00:25 +0200

    sm6150-common: Setup arch flags
    Change-Id: I98624b3b93e3464061fc251f2c9002405db8ae56

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,32 @@ BOARD_VENDOR := samsung
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-03-01
 
+# 64 Bit
+TARGET_USES_64_BIT_BINDER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+# Audio
+USE_XML_AUDIO_POLICY_CONF := 1
+
+
+
+
+
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
--- device.mk
+++ device.mk
@@ -1,3 +1,10 @@
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+
+
 # Doze
 PRODUCT_PACKAGES += \
     SamsungDoze
