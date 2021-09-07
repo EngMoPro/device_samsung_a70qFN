166c0e8f8dd6c3ba028f3554008017441713cd26
EngMoPro <fightstart20000@yahoo.com>
2021-09-07 13:29:33 +0200

    add stock config

--- config.fs
+++ config.fs
@@ -16,8 +16,38 @@ value:2905
 [AID_VENDOR_QRTR]
 value:2906
 
-[AID_VENDOR_THERMAL]
-value:2907
+[AID_VENDOR_SECDIR]
+value:5050
+
+[AID_VENDOR_LOD]
+value:5202
+
+[AID_VENDOR_KNOXCORE]
+value:5250
+
+[AID_VENDOR_SPASS]
+value:5278
+
+[AID_VENDOR_REMOTEDISPLAY]
+value:5386
+
+[AID_VENDOR_AUDIT]
+value:5555
+
+[AID_VENDOR_SYSTEM_RESERVED_STORAGE]
+value:5678
+
+[vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
+
+[system/vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
 
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
@@ -25,11 +55,35 @@ user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/bin/cnss-daemon]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: NET_BIND_SERVICE
+
 [vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE SYS_BOOT
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
 
 [vendor/bin/pd-mapper]
 mode: 0755
@@ -37,25 +91,61 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[system/vendor/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_RADIO
-group: AID_RADIO
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_RADIO
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_RADIO
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -67,7 +157,19 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
+[system/vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
 [vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[system/vendor/bin/loc_launcher]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
@@ -85,69 +187,38 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
 [vendor/firmware_mnt/image/*]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-# Rootfs
-[carrier/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_RADIO
-caps: 0
-
-[dqmdbg/]
-mode: 0770
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[efs/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_RADIO
-caps: 0
-
-[keydata/]
+[bt_firmware/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-[keyrefuge/]
+[persist/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-[metadata/]
+[dsp/]
 mode: 0771
-user: AID_ROOT
-group: AID_SYSTEM
-caps: 0
-
-[optics/]
-mode: 0771
-user: AID_ROOT
-group: AID_SYSTEM
-caps: 0
-
-[omr/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[prism/]
-mode: 0771
-user: AID_ROOT
-group: AID_SYSTEM
-caps: 0
-
-[spu/]
-mode: 0770
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_MEDIA
+group: AID_MEDIA
 caps: 0
